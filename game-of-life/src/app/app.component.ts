f2a4478c1677f37ccdbb33f5d0eafbe952a97b48
Virag Tamas <petymeg@gmail.com>
2022-07-25 21:51:30 +0200

    Added grid generation

--- game-of-life/src/app/app.component.ts
+++ game-of-life/src/app/app.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'game-of-life';
+  rows: number = 30;
+  columns: number = 30;
+  cells: boolean[][] = [];
+
+  constructor() {
+    this.generateCells();
+    console.log(this.cells);
+  }
+
+  generateCells(): void {
+    for (let i: number = 0; i < this.rows; i++) {
+      this.cells.push([]);
+      for (let j: number = 0; j < this.rows; j++) {
+        this.cells[i][j] = false;
+      }
+    }
+  }
+
+  toggleCell(coordinateY: number, coordinateX: number): void {
+    this.cells[coordinateY][coordinateX] =
+      !this.cells[coordinateY][coordinateX];
+  }
 }
--- game-of-life/src/app/app.module.ts
+++ game-of-life/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GridCellComponent } from './grid-cell/grid-cell.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GridCellComponent
   ],
   imports: [
     BrowserModule,
