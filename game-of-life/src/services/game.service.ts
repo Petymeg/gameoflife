400603d35980b72f42dd35ef72bbe468ec1d474f
Petymeg <petymeg@gmail.com>
2022-07-26 10:37:44 +0200

    Generation step and reset methods added

--- game-of-life/src/services/game.service.ts
+++ game-of-life/src/services/game.service.ts
@@ -8,6 +8,7 @@ export class GameService {
   private columnCount: number = 30;
   private cells: boolean[][] = [];
   private generation: number = 0;
+  private initialState: boolean[][] = [];
 
   constructor() {
     this.generateInitialCells();
@@ -33,5 +34,52 @@ export class GameService {
   toggleCell(coordinateY: number, coordinateX: number): void {
     this.cells[coordinateY][coordinateX] =
       !this.cells[coordinateY][coordinateX];
+  }
+
+  nextGeneration(): void {
+    if (this.generation === 0) this.initialState = [...this.cells];
+    this.generation++;
+    this.cells = this.cells.map((row, rowIndex) =>
+      row.map((cell, cellIndex) => {
+        const liveNeighbors = this.countLiveNeighbors(rowIndex, cellIndex);
+        if (cell) {
+          if (liveNeighbors < 2) return !cell;
+          if (liveNeighbors > 3) return !cell;
+        }
+
+        if (!cell && liveNeighbors === 3) return !cell;
+
+        return cell;
+      })
+    );
+  }
+
+  countLiveNeighbors(rowIndex: number, cellIndex: number): number {
+    const neighborCellIndexOffsets: number[][] = [
+      [-1, -1],
+      [-1, 0],
+      [-1, 1],
+      [0, -1],
+      [0, 1],
+      [1, -1],
+      [1, 0],
+      [1, 1],
+    ];
+    let liveNeighbors: number = 0;
+
+    neighborCellIndexOffsets.forEach((offset) => {
+      if (
+        this.cells[rowIndex + offset[0]] &&
+        this.cells[rowIndex + offset[0]][cellIndex + offset[1]]
+      )
+        liveNeighbors++;
+    });
+
+    return liveNeighbors;
+  }
+
+  reset(): void {
+    this.generation = 0;
+    this.cells = [...this.initialState];
   }
 }
