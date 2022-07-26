354dc5fdbdfd11d107701f1cb573231f19ba7173
Petymeg <petymeg@gmail.com>
2022-07-26 10:10:05 +0200

    Angular Material installed

--- game-of-life/src/app/app.module.ts
+++ game-of-life/src/app/app.module.ts
@@ -4,17 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GridCellComponent } from './grid-cell/grid-cell.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    GridCellComponent
-  ],
+  declarations: [AppComponent, GridCellComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
